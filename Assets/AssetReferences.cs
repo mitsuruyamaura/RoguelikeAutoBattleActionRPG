544dbcc576ef624ae407fd300815f174234d3741
mitsuru_yamaura <komayou326@gmail.com>
2022-01-10 13:52:21 +0900

    ユーティリティ用アセットの追加。

---
+++ Assets/AssetReferences.cs
@@ -0,0 +1,32 @@
+/// <summary>
+/// 利用アセット一覧
+/// </summary>
+public class AssetReferences
+{
+    // Main
+    // 2D Beginner: Tutorial Resources(Ruby's Adventure)
+    // https://assetstore.unity.com/packages/templates/tutorials/2d-beginner-tutorial-resources-140167?_ga=2.186125161.1948775146.1641788445-1307585257.1631780292
+
+    // Fantasy Inventory Icons [Free]
+    // https://assetstore.unity.com/packages/2d/gui/icons/fantasy-inventory-icons-free-143805?locale=ja-JP
+
+    // Cartoon FX Free
+    // https://assetstore.unity.com/packages/vfx/particles/cartoon-fx-free-109565
+
+
+    // Utilities
+    // DOTween
+    // https://assetstore.unity.com/packages/tools/animation/dotween-hotween-v2-27676
+
+    // UniRX
+    // https://assetstore.unity.com/packages/tools/integration/unirx-reactive-extensions-for-unity-17276
+
+    // UniTask
+    // https://github.com/Cysharp/UniTask/releases
+
+    // Joystick Pack
+    // https://assetstore.unity.com/packages/tools/input-management/joystick-pack-107631
+
+
+
+}
