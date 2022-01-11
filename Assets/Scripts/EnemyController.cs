18d6ad69cb39f9b98f071357f2b45cb9c99aec59
mitsuru_yamaura <komayou326@gmail.com>
2022-01-11 19:40:16 +0900

    移動時間のランダム化。処理のメソッド化。コメント追加。複数の敵を用意してデバッグ。問題なし。デバッグ完了。

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,36 +7,46 @@ public class EnemyController : ObstacleBase
 {
     [SerializeField]
     private float speed;
-
-    [SerializeField]
-    private bool isVertical;
-
-    [SerializeField]
-    private float chargeTime;
-
+    
     private Rigidbody2D rb;
     private Animator anim;
-    private float timer;
+    private bool isVertical;
+
+    private float moveTimer;
+
     private int direction = 1;
     private int moveChangeCount;
     private int changeCounter;
 
+    /// <summary>
+    /// ‰Šúİ’è
+    /// </summary>
     public override void SetUpObstacleBase() {
         base.SetUpObstacleBase();
 
         TryGetComponent(out rb);
         TryGetComponent(out anim);
 
-        timer = chargeTime;
-        moveChangeCount = Random.Range(1, 5);
+        // ‰ŠúˆÚ“®‚Ì•ûŒü‚ğİ’è
+        isVertical = Random.Range(0, 2) == 0 ? false : true;
+        moveTimer = Random.Range(2, 4);
+
+        SetMoveChangeCount();
     }
 
     void Update() {
-        timer -= Time.deltaTime;
+        ObserveMove();
+    }
 
-        if (timer < 0) {
+    /// <summary>
+    /// ˆÚ“®ŠÔ‚ÆˆÚ“®‰ñ”‚ÌŠÄ‹
+    /// </summary>
+    private void ObserveMove() {
+        moveTimer -= Time.deltaTime;
+
+        if (moveTimer < 0) {
             direction = -direction;
-            timer = chargeTime;
+            moveTimer = Random.Range(2, 4);
             changeCounter++;
             if (changeCounter >= moveChangeCount) {
                 SetMoveChangeCount();
@@ -48,13 +58,19 @@ public class EnemyController : ObstacleBase
     /// ƒ‰ƒ“ƒ_ƒ€‚È•ûŒüØ‚è‘Ö‚¦‚Ìİ’è
     /// </summary>
     private void SetMoveChangeCount() {
-
         isVertical = !isVertical;
         changeCounter = 0;
         moveChangeCount = Random.Range(1, 5);
     }
 
     void FixedUpdate() {
+        MoveEnemy();
+    }
+
+    /// <summary>
+    /// w’è•ûŒü‚Ö‚Ì“G‚Ìã‰º¶‰EˆÚ“®
+    /// </summary>
+    private void MoveEnemy() {
         Vector2 pos = rb.position;
 
         if (isVertical) {
@@ -63,7 +79,7 @@ public class EnemyController : ObstacleBase
             anim.SetFloat("Y", direction);
         } else {
             pos.x = pos.x + Time.deltaTime * speed * direction;
-            anim.SetFloat("X",direction);
+            anim.SetFloat("X", direction);
             anim.SetFloat("Y", 0);
         }
         rb.MovePosition(pos);
