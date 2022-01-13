9784ea85a1f38b6573f0bff55d69474180666c3c
mitsuru_yamaura <komayou326@gmail.com>
2022-01-13 09:31:24 +0900

    Cinemachine によるプレイヤーの追従処理、カメラがゲーム外を映さない処理、任意のタイミングでのカメラのズーム機能、カメラの振動機能の追加。 デバッグ開始。

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using DG.Tweening;
+using UniRx;
+using UniRx.Triggers;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera virtualCamera;
+
+    [SerializeField]
+    private CinemachineImpulseSource impulseSource;
+
+    private float originLensOrthoSize;
+    private float zoomLensOrthoSize = 3.0f;
+    private float zoomDuration = 0.5f;
+
+    private bool isZoom;  // true なら ZoomIn 中
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originLensOrthoSize = virtualCamera.m_Lens.OrthographicSize;
+
+        // デバッグ用
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetKeyDown(KeyCode.Z))
+            .Subscribe(_ => {
+                isZoom = !isZoom;
+                StartCoroutine(ChangeCameraOrthoSize(isZoom ? zoomLensOrthoSize : originLensOrthoSize));
+            });
+
+        // デバッグ用
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetKeyDown(KeyCode.I))
+            .Subscribe(_ => ImpulseCamera());
+    }
+
+    /// <summary>
+    /// カメラの LensOrthoSize を変更したズーム処理
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public IEnumerator ChangeCameraOrthoSize(float value) {
+        DOTween.To(
+            () => virtualCamera.m_Lens.OrthographicSize,
+            x => virtualCamera.m_Lens.OrthographicSize = x,
+            value,
+            zoomDuration
+            );
+        yield return null;
+    }
+
+    /// <summary>
+    /// カメラの振動
+    /// </summary>
+    public void ImpulseCamera() {
+        // カメラ振動
+        impulseSource.GenerateImpulse();
+    }
+}
