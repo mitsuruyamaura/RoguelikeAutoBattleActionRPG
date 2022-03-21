31072b2e935bd19adabcbd948d373ee818f4de6e
mitsuru_yamaura <komayou326@gmail.com>
2022-03-21 12:45:33 +0900

    フードのカウントダウン機能の追加。武器選択中は減少しない。移動・バトルでは減少する。

--- Assets/Scripts/Model/PlayerController.cs
+++ Assets/Scripts/Model/PlayerController.cs
@@ -256,8 +256,7 @@ public class PlayerController : MonoBehaviour
                 GenerateNormalItem();
             }
         }
-
-        // プレゼンター側で削除するので不要
+        // 障害物の削除
         obstacle.DestroyObstacle();
 
         yield return new WaitForSeconds(0.5f);
@@ -266,6 +265,11 @@ public class PlayerController : MonoBehaviour
         CurrentPlayerState.Value = PlayerState.Battle_After;
 
         yield return new WaitForSeconds(0.25f);
+
+        // すぐに武器を取得している場合には、移動状態にはしない
+        if (currentPlayerState == PlayerState.Info || CurrentPlayerState.Value == PlayerState.Info) {
+            yield break;
+        }
 
         currentPlayerState = PlayerState.Move;
         CurrentPlayerState.Value = PlayerState.Move;
